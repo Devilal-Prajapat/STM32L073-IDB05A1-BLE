3ac3abd831ca71bb9056fb3f3087de6be2a76e5f
Devilal Prajapat <13392395+Devilal-Prajapat@users.noreply.github.com>
2022-02-03 12:58:42 +0530

    Add project code

---
+++ Core/Inc/services.h
@@ -0,0 +1,10 @@
+#include "bluenrg_aci_const.h"
+
+
+tBleStatus Add_Led_Service(void);
+
+tBleStatus Add_Time_Service(void);
+
+void Update_Time_Characteristics(void) ;
+
+void user_notify(void *pData);
